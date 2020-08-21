e90ce89ee6d4dc87b1feee5c655f47de23a40b8b
Saugat17Dhakal <59152157+Saugat17Dhakal@users.noreply.github.com>
2020-08-21 09:45:44 +0545

    Portfolio and Carousel Scripts added

--- src/js/script.js
+++ src/js/script.js
@@ -94,9 +94,107 @@ class TypeWriter {
     new TypeWriter(txtElement, words, wait);
   }
 
-  // Carousel
-  var flkty = new Flickity( '.main-gallery', {
-    // options
-    contain: true,
-    autoplay: true
-  });
+// Counter
+
+
+const counters = document.querySelectorAll('.counter');
+const speed = 100; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+	
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
+
+
+// Carousel
+
+// Declare Variables
+
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+display(index);
+function display (index) {
+	slides.forEach((slide) => {
+		slide.style.display = 'none';
+	});
+	slides[index].style.display = 'flex';
+}
+// Go to Next slide
+function nextSlide () {
+	index++;
+	if (index > slides.length - 1) {
+		index = 0;
+	}
+	display(index);
+}
+
+// Go to Prev slide
+function prevSlide () {
+	index--;
+	if (index < 0) {
+		index = slides.length - 1;
+	}
+	display(index);
+}
+
+// Click for next or prvious slide
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+
+// Portfolio Filter
+
+const buttons=document.querySelector("#buttons").children;
+const items=document.querySelector(".items").children;
+
+ 
+ for(let i=0; i<buttons.length; i++){
+   buttons[i].addEventListener("click",function(){
+
+      for(let j=0; j<buttons.length; j++){
+        buttons[j].classList.remove("active")
+      }
+      this.classList.add("active")
+      const target=this.getAttribute("data-target");
+
+      for(let k=0; k<items.length; k++){
+              items[k].style.opacity="0.5";
+              items[k].style.transform="scale(0.8)";
+
+
+              if(items[k].getAttribute("data-id")==target){
+                items[k].style.opacity="1";
+              items[k].style.transform="scale(1)";
+              }
+              if(target=="all"){
+                items[k].style.opacity="1";
+              items[k].style.transform="scale(1)";
+              }
+      }
+   })
+ }
+
+
